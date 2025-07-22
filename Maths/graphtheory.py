6c6cf336004aaf21ccb75f5387f96648555c00e2
Vyonavee <vyonanjeri62@gmail.com>
2025-07-22 22:02:50 +0300

    Fixed indentation'

--- Maths/graphtheory.py
+++ Maths/graphtheory.py
@@ -10,10 +10,10 @@ graph={
 def dfs(graph,start,visited=None):
     if visited is None:
         visited=set()
-        visited.add(start)
-        print(start,end=" ")
-        for neighbor in graph[start]:
-          if neighbor not in visited:
+    visited.add(start)
+    print(start,end=" ")
+    for neighbor in graph[start]:
+        if neighbor not in visited:
             dfs(graph,neighbor,visited)
-        
-        dfs(graph,"A")
+
+dfs(graph,"A")
